360dc79a920bdffc8d17f8ca73152414dc741dc6
Anil Bodaballa <yourgithubemail@gmail.com>
2026-05-09 11:21:32 +0530

    Add routing structure and protected routes for the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,16 @@
 import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import SearchResults from "./pages/SearchResults";
+import FlashDeals from "./pages/FlashDeals";
+import Checkout from "./pages/Checkout";
+import MyOrders from "./pages/MyOrders";
+import OrderTracking from "./pages/OrderTracking";
+import Addresses from "./pages/Addresses";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -15,6 +27,25 @@ const App = () => {
           },
         }}
       />
+
+      <Routes>
+        {/* Auth pages- No Navbar/Footer */}
+        <Route path="/login" element={<Login />} />
+        {/* Main pages - With Navbar/Footer */}
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/deals" element={<FlashDeals />} />
+          {/* Protected Route */}
+          <Route element={<ProtectedRoute/>}>
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<MyOrders />} />
+            <Route path="/order/:id" element={<OrderTracking />} />
+            <Route path="/addresses" element={<Addresses />} />
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 };
