f96950561c6d37946cce0987458449d30ffb2811
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-06 19:00:14 +0530

    Implement search functionality in SearchResults component

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../types";
+import { useSearchParams } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 const SearchResults = () => {
-  return (
-    <div>SearchResults</div>
-  )
-}
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchparams] = useSearchParams();
+  const query = searchparams.get("q") || "";
 
-export default SearchResults
+  useEffect(() => {
+    if (!query) return;
+    setLoading(true);
+    setProducts(
+      dummyProducts.filter((p: any) =>
+        p.name.toLowerCase().includes(query.toLowerCase()),
+      ),
+    );
+    setLoading(false);
+  }, [query]);
+  return <div>SearchResults</div>;
+};
+
+export default SearchResults;
