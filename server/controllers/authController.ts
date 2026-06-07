99184921eea0da751509e04b4d9e0db0fe541993
Anil Bodaballa <yourgithubemail@gmail.com>
2026-06-07 17:01:28 +0530

    Create Register controller function

---
+++ server/controllers/authController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+import bcrypt from "bcrypt";
+
+// Register
+// POST /api/auth/register
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ messsage: "Please provide all fields" });
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email: email.toLowerCase() },
+  });
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "User Already exist with this email" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: { name, email: email.toLowerCase(), password: hashedPassword },
+  });
+};
