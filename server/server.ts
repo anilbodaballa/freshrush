84969324ef5834e90a89e886d4548a91afb6ef33
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-07 18:52:44 +0530

    Error handling function

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 
@@ -15,6 +15,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 app.use("/api/auth", authRouter);
+
+// Error handling
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  res.status(500).json({ message: error.message });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
