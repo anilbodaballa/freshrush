43433e276f47cd04e48e9fa247f8418df7c2da0c
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-09 17:54:58 +0530

    feat: integrate Inngest for event handling and add hello-world function

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,8 @@ import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRotes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
@@ -21,7 +23,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/orders", orderRouter);
-
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   console.log(error);
