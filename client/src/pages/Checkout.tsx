3ac370685fef34b8a870de39514953322fea8dfb
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-15 21:25:21 +0530

    Enhance checkout process with order placement functionality and address population

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import { dummyAddressData } from "../assets/assets";
 import { useState } from "react";
 import type { Address } from "../types";
 import {
@@ -13,13 +12,16 @@ import {
 import CheckoutAddress from "../components/Checkout/CheckoutAddress";
 import CheckoutPayment from "../components/Checkout/CheckoutPayment";
 import CheckoutReview from "../components/Checkout/CheckoutReview";
+import api from "../config/api";
+import toast from "react-hot-toast";
+import { useAuth } from "../context/AuthContext";
 
 const Checkout = () => {
   const navigate = useNavigate();
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
 
-  const { items, cartTotal } = useCart();
-  const { user } = { user: { addresses: dummyAddressData } };
+  const { items, cartTotal, clearCart } = useCart();
+  const { user } = useAuth();
 
   const [step, setStep] = useState("address");
   const [loading, setLoading] = useState(false);
@@ -50,7 +52,32 @@ const Checkout = () => {
 
   const handlePlaceOrder = async () => {
     setLoading(true);
-    navigate("/orders");
+    try {
+      const orderData = {
+        items: items.map((item) => ({
+          product: item.product.id,
+          quantity: item.quantity,
+        })),
+        shippingAddress: address,
+        paymentMethod,
+      };
+
+      const { data } = await api.post("/orders", orderData);
+      console.log(data);
+
+      if (data.url) {
+        window.location.href = data.url;
+        return;
+      }
+      clearCart();
+      toast.success("Order placed successfully");
+      navigate(`/orders/${data.order.id}`);
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || error.message);
+    } finally {
+      setLoading(false);
+      scrollTo(0, 0);
+    }
   };
 
   // Populate address from user's default address
