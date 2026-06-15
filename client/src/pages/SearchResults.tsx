03dc13074e46c0c6a3cd054f32e67a12c625e541
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-15 13:29:09 +0530

    Completed Search product functionality

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -5,6 +5,8 @@ import { dummyProducts } from "../assets/assets";
 import { Home, Search } from "lucide-react";
 import Loading from "../components/Loading";
 import ProductCard from "../components/ProductCard";
+import api from "../config/api";
+import toast from "react-hot-toast";
 
 const SearchResults = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -15,12 +17,13 @@ const SearchResults = () => {
   useEffect(() => {
     if (!query) return;
     setLoading(true);
-    setProducts(
-      dummyProducts.filter((p: any) =>
-        p.name.toLowerCase().includes(query.toLowerCase()),
-      ),
-    );
-    setLoading(false);
+    api
+      .get(`/products?search=${encodeURIComponent(query)}`)
+      .then((res) => setProducts(res.data.products))
+      .catch((error: any) => {
+        toast.error(error.response?.data?.message || error.message);
+      })
+      .finally(() => setLoading(false));
   }, [query]);
   return (
     <div className="min-h-screen bg-app-cream">
