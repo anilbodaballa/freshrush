5e26a950d50fdbe4e73690a119cbb064cf8524fd
anilbodaballa <152702276+anilbodaballa@users.noreply.github.com>
2026-06-20 17:57:20 +0530

    Add event for payment_intent.canceled and payment_intent.payment_failed

--- server/controllers/webhooks.ts
+++ server/controllers/webhooks.ts
@@ -68,11 +68,22 @@ export const stripeWebhook = async (request: Request, response: Response) => {
         }
 
         break;
-      case "payment_method.attached":
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
+      case "payment_intent.canceled":
+      case "payment_intent.payment_failed": {
+        const paymentIntentFailure = event.data.object as Stripe.PaymentIntent;
+        const paymentIntendFailureId = paymentIntentFailure.id;
+
+        //   Getting Session Metadata
+        const sessionFailure = await stripe.checkout.sessions.list({
+          payment_intent: paymentIntendFailureId,
+        });
+
+        const failureOrderId = (sessionFailure.data[0].metadata as any).orderId;
+
+        await prisma.order.delete({ where: { id: failureOrderId } });
         break;
+      }
+
       // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
